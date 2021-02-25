88a57161b50bb06e5709600a8a0a487e2b9ac098
Mark Sweitzer <marksweitzer@Marks-2020-MBP.fios-router.home>
2021-02-25 13:43:20 -0500

    added entityDropDown, simpleProduct, and parallax modules

--- src/app.js
+++ src/app.js
@@ -1,4 +1,12 @@
+// imports. 
 import test from './general';
+import entityDropDown from './_aspdnsf/entityDropDown';
+import simpleProduct from './_aspdnsf/simpleProduct';
+import parallax from './parallax/parllax';
+
 export {
-    test
+    test, 
+    simpleProduct, 
+    entityDropDown,
+    parallax
 }
