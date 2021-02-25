0910118e5fc8b12d114241db66c4f75dd345935f
Mark Sweitzer <marksweitzer@Marks-2020-MBP.fios-router.home>
2021-02-25 13:45:23 -0500

    removed not needed vars from parallax

--- src/parallax/parllax.js
+++ src/parallax/parllax.js
@@ -1,17 +1,13 @@
 function parallax(){
     window.addEventListener('scroll', function(e){
-        let parallaxCons = document.querySelectorAll('.parallax-group');
         let parallaxEls = document.querySelectorAll('.parallax');
-        let yScrolledDist = window.scrollY;
         if(parallaxEls.length > 0){
             parallaxEls.forEach((cur, idx)=>{
                 let container = cur.parentNode;
                 let containerHeight = container.offsetHeight;
                 let spaceToTop = container.getBoundingClientRect().top;
                 if((spaceToTop <= 0) && (spaceToTop > -containerHeight)){
-                    let sectionScrolledDistance;  
                     let rateY = spaceToTop * cur.dataset.yscroll;
-                    // console.log('RATE Y:', rateY);
                     switch(cur.dataset.direction){
                         case 'vertical' : 
                         cur.style.transform = `translate3d(0px, ${rateY}px, 0px)`;
