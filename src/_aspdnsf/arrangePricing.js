9a5407ef962c222a729d9f1ecd72e79a93f52f18
Mark Sweitzer <marksweitzer@Marks-2020-MBP.fios-router.home>
2021-02-26 10:04:42 -0500

    arrangePricing function caused failure in updateEntityDropDownLayout... Reverted temporarily

--- src/_aspdnsf/arrangePricing.js
+++ src/_aspdnsf/arrangePricing.js
@@ -1,9 +1,7 @@
 const arrangePricing = () => {
-    let descriptionColumn = document.getElementById('product-description-column');
-    let variantInformation = descriptionColumn.childNodes[1].childNodes[5];
     let variantPricing = variantInformation.childNodes[1].childNodes[1];
     let options = descriptionColumn.childNodes[1].childNodes[3];
-    let gridImages = Array.from(document.getElementsByClassName('grid-item-image-wrap'));
+
     descriptionColumn.style.position="relative";
     // changes the order of the elements than from original default
     descriptionColumn.prepend(options);
